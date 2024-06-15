6d497d15ed5427d1eb5fcd26da1fe5bc17c580a1
Tegar Naufal Hanip <tegarnaufalh@gmail.com>
2024-06-15 11:16:47 +0700

    add gitignore, update title, add feature add/remove cashflow, add separator input

--- src/InvestmentCalculator.jsx
+++ src/InvestmentCalculator.jsx
@@ -7,25 +7,43 @@ const InvestmentCalculator = () => {
   const [roiResult, setRoiResult] = useState(null);
   const [piResult, setPiResult] = useState(null);
 
+  const formatNumber = (value) => {
+    return value.replace(/\D/g, '').replace(/\B(?=(\d{3})+(?!\d))/g, '.');
+  };
+
+  const handleInvestmentChange = (value) => {
+    setInvestment(formatNumber(value));
+  };
+
   const handleCashFlowChange = (index, value) => {
     const newCashFlows = [...cashFlows];
-    newCashFlows[index] = value;
+    newCashFlows[index] = formatNumber(value);
     setCashFlows(newCashFlows);
   };
 
+  const addCashFlow = () => {
+    setCashFlows([...cashFlows, '']);
+  };
+
+  const removeCashFlow = () => {
+    if (cashFlows.length > 1) {
+      setCashFlows(cashFlows.slice(0, -1));
+    }
+  };
+
   const calculateROI = () => {
-    const totalCashFlows = cashFlows.reduce((acc, val) => acc + parseFloat(val || 0), 0);
-    const roi = ((totalCashFlows - parseFloat(investment)) / parseFloat(investment)) * 100;
+    const totalCashFlows = cashFlows.reduce((acc, val) => acc + parseFloat(val.replace(/\./g, '') || 0), 0);
+    const roi = ((totalCashFlows - parseFloat(investment.replace(/\./g, ''))) / parseFloat(investment.replace(/\./g, ''))) * 100;
     setRoiResult(roi.toFixed(2));
   };
 
   const calculatePI = () => {
     const discountRateDecimal = parseFloat(discountRate) / 100;
     const presentValue = cashFlows.reduce((acc, val, index) => {
-      const discountedValue = parseFloat(val) / Math.pow(1 + discountRateDecimal, index + 1);
+      const discountedValue = parseFloat(val.replace(/\./g, '')) / Math.pow(1 + discountRateDecimal, index + 1);
       return acc + discountedValue;
     }, 0);
-    const pi = presentValue / parseFloat(investment);
+    const pi = presentValue / parseFloat(investment.replace(/\./g, ''));
     setPiResult(pi.toFixed(2));
   };
 
@@ -40,31 +58,45 @@ const InvestmentCalculator = () => {
       <div>
         <label>
           Investment Amount:
-          <input type="number" value={investment} onChange={(e) => setInvestment(e.target.value)} />
+          <input
+            type="text"
+            value={investment}
+            onChange={(e) => handleInvestmentChange(e.target.value)}
+          />
         </label>
       </div>
       <div>
         <label>
           Discount Rate (%):
-          <input type="number" value={discountRate} onChange={(e) => setDiscountRate(e.target.value)} />
+          <input
+            type="number"
+            value={discountRate}
+            onChange={(e) => setDiscountRate(e.target.value)}
+          />
         </label>
       </div>
       <div>
-        <label>
-          Cash Flows:
-          {cashFlows.map((flow, index) => (
+        <label>Cash Flows:</label>
+        {cashFlows.map((flow, index) => (
+          <div key={index}>
+            <label>Year {index + 1}</label>
             <input
-              key={index}
-              type="number"
+              type="text"
               value={flow}
               onChange={(e) => handleCashFlowChange(index, e.target.value)}
             />
-          ))}
-        </label>
+          </div>
+        ))}
+        <div>
+          <button onClick={addCashFlow}>Add Cash Flow</button>
+          <button onClick={removeCashFlow}>Remove Cash Flow</button>
+        </div>
       </div>
       <button onClick={handleCalculate}>Calculate</button>
       {roiResult !== null && <div>ROI: {roiResult}%</div>}
       {piResult !== null && <div>PI: {piResult}</div>}
+
+      <div>Created by: Muhammad Fadhil Abidin, Robby Nugraha, Tegar Naufal Hanip</div>
     </div>
   );
 };
